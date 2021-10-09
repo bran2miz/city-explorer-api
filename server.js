8a27234c38384d85764e0ca8902ab877b82d1bd5
brandonmizutani <bran2miz@gmail.com>
2021-10-09 16:51:25 -0700

    localhost api finally working for the weather.json cities

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const express= require('express');
+const express = require('express');
 require('dotenv').config();
-const cors= require('cors');
+const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+// const axios = require('axios');
 
+const PORT = process.env.PORT || 3001;
 const weatherData = require('./data/weather.json');
 
 class Forecast {
   constructor(date, description) {
     this.date = date;
-    this.description =description;
+    this.description = description;
   }
 }
 
@@ -21,8 +22,9 @@ app.get('/', (request, response) => {
   response.status(200).send('goto: localhost:3001/weather');
 });
 
-app.get('/weather', (request, response)=> {
-  let weatherArray= [];
+// WEATHER
+app.get('/weather', (request, response) => {
+  let weatherArray = [];
   let lat = request.query.lat;
   let lon = request.query.lon;
   const weatherResponse = weatherData.find(
@@ -32,16 +34,21 @@ app.get('/weather', (request, response)=> {
       citySearched.lon === lon
   );
 
-  if(weatherResponse) {
+  if (weatherResponse) {
     weatherArray = weatherResponse.data.map(
-      forecast => 
+      forecast =>
         new Forecast(forecast.valid_date, forecast.weather.description)
     );
     response.status(200).send(weatherArray);
-  }
-  else {
-    response.status(400).send('Unable to locate city');
+  } else {
+    response.status(400).send('City not found');
   }
 });
+
+// Errors
+app.get('*', errorHandler);
+function errorHandler(request, response) {
+  response.status(500).send('Something went wrong');
+}
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
