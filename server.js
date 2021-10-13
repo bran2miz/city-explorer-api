b925f76cb542a23b0db2af8d1103e47ec8834664
brandonmizutani <bran2miz@gmail.com>
2021-10-12 19:05:12 -0700

    added description to my rendered weather forecast api

--- server.js
+++ server.js
@@ -3,13 +3,16 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const weatherData = require('./data/weather.json');
+// Like REACT: import weather from './data/weather.json'
+
 const app = express();
 app.use(cors());
 
 // const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
-const weatherData = require('./data/weather.json');
+
 
 class Forecast {
   constructor(date, description) {
@@ -18,18 +21,20 @@ class Forecast {
   }
 }
 
+//Setup Routes (Root)
 app.get('/', (request, response) => {
   response.status(200).send('goto: localhost:3001/weather');
 });
 
 // WEATHER
+
 app.get('/weather', (request, response) => {
   let weatherArray = [];
   let lat = request.query.lat;
   let lon = request.query.lon;
   const weatherResponse = weatherData.find(
     citySearched =>
-      citySearched.city_name === request.query.searchQuery &&
+      citySearched.city_name.toLowerCase() === request.query.searchQuery.toLowerCase() &&
       citySearched.lat === lat &&
       citySearched.lon === lon
   );
@@ -45,10 +50,11 @@ app.get('/weather', (request, response) => {
   }
 });
 
-// Errors
+// Errors (at the end if no routes match)
 app.get('*', errorHandler);
 function errorHandler(request, response) {
   response.status(500).send('Something went wrong');
 }
 
+//Listen on the port for requests from the client
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
